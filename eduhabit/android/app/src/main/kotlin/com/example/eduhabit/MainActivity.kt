800472ba76939d8c1dc01606bb6248be8ecb1640
inshad02 <inshadkorambayil@gmail.com>
2025-03-25 19:42:15 +0530

    nothing

--- eduhabit/android/app/src/main/kotlin/com/example/eduhabit/MainActivity.kt
+++ eduhabit/android/app/src/main/kotlin/com/example/eduhabit/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.eduhabit
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
--- eduhabit/lib/routes/app_pages.dart
+++ eduhabit/lib/routes/app_pages.dart
@@ -1,16 +1,10 @@
-
+import 'package:eduhabit/views/auth/login_view_email.dart';
 import 'package:get/get.dart';
 
 import '../bindings/auth_binding.dart';
-import '../views/login_view.dart';
-import 'app_routes.dart';
 
 class AppPages {
   static final routes = [
-    GetPage(
-      name: '/login',
-      page: () => LoginView(),
-      binding: AuthBinding(),
-    ),
+    GetPage(name: '/login', page: () => LoginEmail(), binding: AuthBinding()),
   ];
 }
