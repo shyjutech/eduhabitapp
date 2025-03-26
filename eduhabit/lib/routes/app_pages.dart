348a1b4ae1ffd974fa5780069463a577e0bbfe7d
shyjutech <shyjutalks@gmail.com>
2025-03-26 19:50:50 +0530

    Revert "On boarding screen"

--- eduhabit/lib/routes/app_pages.dart
+++ eduhabit/lib/routes/app_pages.dart
@@ -2,22 +2,9 @@ import 'package:eduhabit/views/auth/login_view_email.dart';
 import 'package:get/get.dart';
 
 import '../bindings/auth_binding.dart';
-import '../views/login_view.dart';
-import '../views/onboarding_view.dart';
-import 'app_routes.dart';
 
 class AppPages {
   static final routes = [
-    GetPage(
-      name: '/login',
-      page: () => LoginView(),
-      binding: AuthBinding(),
-    ),
-    GetPage(
-      name: '/onboarding',
-      page: () => const OnboardingView(),
-      binding: AuthBinding(),
-    ),
     GetPage(name: '/login', page: () => LoginEmail(), binding: AuthBinding()),
   ];
 }
