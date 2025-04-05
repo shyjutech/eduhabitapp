c6e04e53301865fe35d58ff1235e3fe19667c200
Jishad Melamadathil <jishadmm40@gmail.com>
2025-04-05 06:40:38 +0530

    firebase store users data

--- eduhabit/lib/controllers/auth_controller.dart
+++ eduhabit/lib/controllers/auth_controller.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthController extends GetxController {
   final auth = FirebaseAuth.instance;
@@ -12,11 +13,16 @@ class AuthController extends GetxController {
 
   Future<void> login() async {
     _isLoading.value = true;
+    final FirebaseFirestore _firestore = FirebaseFirestore.instance;
     try {
       final userCredential = await auth.createUserWithEmailAndPassword(
         email: emailController.text,
         password: passwordController.text,
       );
+      await _firestore.collection('user').add({
+        'email': emailController.text,
+        'password': passwordController.text,
+      });
       Get.snackbar('Success', 'Logged in success');
       Get.offAll('/dashboard');
     } on FirebaseAuthException catch (e) {
@@ -26,10 +32,13 @@ class AuthController extends GetxController {
         Get.snackbar('Error', 'This email is already exist');
       }
     } catch (e) {
-      print('Error: $e');
+      Get.snackbar('Error',' $e');
     } finally {
       _isLoading.value = false;
     }
   }
+
 }
+
+
 
