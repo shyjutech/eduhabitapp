e177eeaa4a3f4433a61da5dbfa01404784af6f99
Geo Paulson <geopaul94@gmail.com>
2025-04-19 18:53:00 +0530

    signup view completed , signup authentication , navigation from login screen to signup screen completed

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'EduHabit',
-      initialRoute: '/signup',
+      initialRoute: '/login',
       getPages: AppPages.routes,
       theme: AppThemes.lightTheme,
       darkTheme: AppThemes.darkTheme,
